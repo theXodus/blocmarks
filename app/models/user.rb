5f1316e4978537815b8ec5fb2473a770c456f282
theXodus <jprice1990@gmail.com>
2016-05-13 12:58:34 -0700

    Add username to Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,27 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   #, :lockable, :timeoutable and :omniauthable
+  attr_accessor :login
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  validate :validate_username
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      conditions[:email].downcase! if conditions[:email]
+      where(conditions.to_hash).first
+    end
+  end
+
+  private
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
 end
